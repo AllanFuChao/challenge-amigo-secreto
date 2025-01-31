15d3401c1e4a17a4f129cae9d6b561dae4f7c4a9
afuc <afuc@usma.com.pa>
2025-01-31 12:06:33 -0500

    Imprementar la variable de amigos y la funcion para agregar nuevos amigos a la variable.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+console.log(amigos);
+
+// Caputa el valor del campo de entrada
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value;
+
+    // Valida la entrada
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre valido.");
+        return;
+    }
+
+    // Actualiza el array de 'amigos'
+    amigos.push(nombre);
+
+    // Limpia el campo de entrada
+    input.value = "";
+}
+
+
+
+
+
+
+
