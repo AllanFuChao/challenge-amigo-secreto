b1c6044b9c666a75d1f8f66ddeddec4359942a31
afuc <afuc@usma.com.pa>
2025-02-03 14:52:00 -0500

    Finalizar el proyecto de amigo-secreto

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-console.log(amigos);
+// console.log(amigos);
 
 // Caputa el valor del campo de entrada
 function agregarAmigo() {
     let input = document.getElementById("amigo");
     let nombre = input.value;
 
-    // Valida la entrada
+    // Valida la entrada, si no se ingresa nada, no seguira la funcion.
     if (nombre === "") {
         alert("Por favor, inserte un nombre valido.");
         return;
@@ -39,6 +39,25 @@ function actualizarLista() {
         lista.appendChild(li);
     }
 }
+
+function sortearAmigo() {
+    // Confirmar que haya un listado de amigos, si no hay, no seguira la funcion.
+    if (amigos.length === 0) {
+        alert("Agregue nombres a la lista de amigos.");
+        return;
+    }
+
+    // codigo para que eliga aleatoriamente un amigo de la lista.
+    let eleccionAleatoria = Math.floor(Math.random() * amigos.length);
+
+    // Define el nombre del amigo.
+    let amigoSorteado = amigos[eleccionAleatoria];
+
+    // Muestra el resultado en el documento .html
+    document.getElementById("resultado").innerHTML = `<li>${amigoSorteado}</li>`;
+    // console.log(amigoSorteado);
+}
+
 
 
 
